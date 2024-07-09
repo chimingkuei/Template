2c573f6ff2c6685be1a84b18c80ea2d9368e0919
chimingkuei <chimingkuei@gmail.com>
2024-07-10 01:08:00 +0800

    Template

--- TitanVision/Recognition.cs
+++ TitanVision/Recognition.cs
@@ -1,6 +1,7 @@
 ﻿using OpenCvSharp;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -8,7 +9,12 @@ using System.Threading.Tasks;
 
 namespace TitanVision
 {
-    public class Recognition
+    interface IFileManager
+    {
+        void BatchImageTransform(string folderpath, string file_extension, double threshold, Func<Mat, double, Mat> fun);
+    }
+
+    public class Recognition : IFileManager
     {
         public Mat Binarization(Mat src, double threshold)
         {
@@ -110,9 +116,17 @@ namespace TitanVision
             }
         }
 
+        public void BatchImageTransform(string folderpath, string file_extension, double threshold, Func<Mat, double, Mat> fun)
+        {
+            string[] imageFiles = Directory.GetFiles(folderpath, file_extension);
+            foreach (string imageFile in imageFiles)
+            {
+                Mat image = Cv2.ImRead(imageFile, ImreadModes.Color);
+                Mat dst = fun(image, threshold);
+                Cv2.ImWrite(Path.Combine(@"E:\DIP Temp\Image Output", DateTime.Now.ToString("yyyyMMddHHmmssfff")+".bmp"), dst);
+            }
+        }
+
         
-
-
-
     }
 }
