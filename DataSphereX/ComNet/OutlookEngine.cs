a35348f09809b3b2698c72938de3369e2f770c6b
chimingkuei <chimingkuei@gmail.com>
2024-07-23 15:40:42 +0800

    Template

--- DataSphereX/ComNet/OutlookEngine.cs
+++ DataSphereX/ComNet/OutlookEngine.cs
@@ -12,7 +12,23 @@ namespace DataSphereX.ComNet
 {
     public class OutlookEngine
     {
-        public void SendEmail1(string recipient, string subject, string content)
+        public string recipient { get; set; }
+        public string subject { get; set; }
+        public string shipper { get; set; }
+        public OutlookEngine(string _recipient, string _subject)
+        {
+            recipient = _recipient;
+            subject = _subject;
+        }
+
+        public OutlookEngine(string _shipper, string _recipient, string _subject)
+        {
+            shipper = _shipper;
+            recipient = _recipient;
+            subject = _subject;
+        }
+
+        public void SendEmail1(string content)
         {
             // 建立Outlook Application物件
             Application outlookApp = new Application();
@@ -26,20 +42,21 @@ namespace DataSphereX.ComNet
             mail.Send();
         }
 
-        public void SendEmail2()
+        public void SendEmail2(string content)
         {
-            SmtpClient client = new SmtpClient("mail.deep-wise.com.tw", 25); // 替換為實際的 SMTP 伺服器和端口
+            // 替換為實際的 SMTP 伺服器和端口
+            SmtpClient client = new SmtpClient("mail.deep-wise.com.tw", 25); 
 
             // 設置 SMTP 用戶名和密碼
-            client.Credentials = new NetworkCredential("chimingkuei@deep-wise.com.tw", "7@2wbxD6"); // 替換為實際的用戶名和密碼
+            client.Credentials = new NetworkCredential("chimingkuei@deep-wise.com.tw", "7@2wbxD6");
             client.EnableSsl = false;
 
             // 創建郵件對象
             MailMessage mail = new MailMessage();
-            mail.From = new MailAddress("chimingkuei@deep-wise.com.tw"); // 替換為發件人地址
-            mail.To.Add("chimingkuei@deep-wise.com.tw"); // 替換為收件人地址
-            mail.Subject = "測試郵件";
-            mail.Body = "這是一封測試郵件。";
+            mail.From = new MailAddress(shipper);
+            mail.To.Add(recipient);
+            mail.Subject = subject;
+            mail.Body = content;
             client.Send(mail);
         }
     }
