3bb19a1d5b4a2905765d521083110356c0c7eaf1
chimingkuei <chimingkuei@gmail.com>
2025-06-15 00:01:41 +0800

    Template

---
+++ DataNexus/ComNet/FTPClientEngine.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataNexus
+{
+    public class FTPClientEngine
+    {
+        public string user { get; set; }
+        public string password { get; set; }
+        public string rootURL { get; set; }
+        public string ip { get; set; }
+        public string port { get; set; }
+
+        public FTPClientEngine(string _User, string _password, string _ip, string _port)
+        {
+            user = _User;
+            password = _password;
+            ip = _ip;
+            port = _port;
+            rootURL = "ftp://" + ip + (string.IsNullOrEmpty(_port) ? "" : ":" + port);
+        }
+
+        public bool FileUpload(FileInfo localFile, string ftpPath, string ftpFileName)
+        {
+            bool success = false;
+            FtpWebRequest ftpWebRequest = null;
+            FileStream localFileStream = null;
+            Stream requestStream = null;
+            try
+            {
+                string uri = rootURL + ftpPath + ftpFileName;
+                ftpWebRequest = (FtpWebRequest)FtpWebRequest.Create(new Uri(uri));
+                ftpWebRequest.Credentials = new NetworkCredential(user, password);
+                ftpWebRequest.UseBinary = true;
+                ftpWebRequest.KeepAlive = false;
+                ftpWebRequest.Method = WebRequestMethods.Ftp.UploadFile;
+                ftpWebRequest.ContentLength = localFile.Length;
+                int buffLength = 2048;
+                byte[] buff = new byte[buffLength];
+                int contentLen;
+                localFileStream = localFile.OpenRead();
+                requestStream = ftpWebRequest.GetRequestStream();
+                contentLen = localFileStream.Read(buff, 0, buffLength);
+                while (contentLen != 0)
+                {
+                    requestStream.Write(buff, 0, contentLen);
+                    contentLen = localFileStream.Read(buff, 0, buffLength);
+                }
+                success = true;
+            }
+            catch (Exception)
+            {
+                success = false;
+            }
+            finally
+            {
+                if (requestStream != null)
+                {
+                    requestStream.Close();
+                }
+                if (localFileStream != null)
+                {
+                    localFileStream.Close();
+                }
+            }
+            return success;
+        }
+
+        public bool FileDownload(string localPath, string localFileName, string ftpPath, string ftpFileName)
+        {
+            bool success = false;
+            FtpWebRequest ftpWebRequest = null;
+            FtpWebResponse ftpWebResponse = null;
+            Stream ftpResponseStream = null;
+            FileStream outputStream = null;
+            try
+            {
+                outputStream = new FileStream(localPath + localFileName, FileMode.Create);
+                string uri = rootURL + ftpPath + ftpFileName;
+                ftpWebRequest = (FtpWebRequest)FtpWebRequest.Create(new Uri(uri));
+                ftpWebRequest.Credentials = new NetworkCredential(user, password);
+                ftpWebRequest.UseBinary = true;
+                ftpWebRequest.Method = WebRequestMethods.Ftp.DownloadFile;
+                ftpWebResponse = (FtpWebResponse)ftpWebRequest.GetResponse();
+                ftpResponseStream = ftpWebResponse.GetResponseStream();
+                long contentLength = ftpWebResponse.ContentLength;
+                int bufferSize = 2048;
+                byte[] buffer = new byte[bufferSize];
+                int readCount;
+                readCount = ftpResponseStream.Read(buffer, 0, bufferSize);
+                while (readCount > 0)
+                {
+                    outputStream.Write(buffer, 0, readCount);
+                    readCount = ftpResponseStream.Read(buffer, 0, bufferSize);
+                }
+                success = true;
+            }
+            catch (Exception)
+            {
+                success = false;
+            }
+            finally
+            {
+                if (outputStream != null)
+                {
+                    outputStream.Close();
+                }
+                if (ftpResponseStream != null)
+                {
+                    ftpResponseStream.Close();
+                }
+                if (ftpWebResponse != null)
+                {
+                    ftpWebResponse.Close();
+                }
+            }
+            return success;
+        }
+
+        public bool FileDelete(string ftpPath, string ftpName)
+        {
+            bool success = false;
+            FtpWebRequest ftpWebRequest = null;
+            FtpWebResponse ftpWebResponse = null;
+            Stream ftpResponseStream = null;
+            StreamReader streamReader = null;
+            try
+            {
+                string uri = rootURL + ftpPath + ftpName;
+                ftpWebRequest = (FtpWebRequest)FtpWebRequest.Create(new Uri(uri));
+                ftpWebRequest.Credentials = new NetworkCredential(user, password);
+                ftpWebRequest.KeepAlive = false;
+                ftpWebRequest.Method = WebRequestMethods.Ftp.DeleteFile;
+                ftpWebResponse = (FtpWebResponse)ftpWebRequest.GetResponse();
+                long size = ftpWebResponse.ContentLength;
+                ftpResponseStream = ftpWebResponse.GetResponseStream();
+                streamReader = new StreamReader(ftpResponseStream);
+                string result = String.Empty;
+                result = streamReader.ReadToEnd();
+
+                success = true;
+            }
+            catch (Exception)
+            {
+                success = false;
+            }
+            finally
+            {
+                if (streamReader != null)
+                {
+                    streamReader.Close();
+                }
+                if (ftpResponseStream != null)
+                {
+                    ftpResponseStream.Close();
+                }
+                if (ftpWebResponse != null)
+                {
+                    ftpWebResponse.Close();
+                }
+            }
+            return success;
+        }
+
+        /// <summary>
+        /// If the function returns false, it indicates that the file does not exist.
+        /// </summary>
+        public bool FileCheckExist(string ftpPath, string ftpName)
+        {
+            bool success = false;
+            FtpWebRequest ftpWebRequest = null;
+            WebResponse webResponse = null;
+            StreamReader reader = null;
+            try
+            {
+                string url = rootURL + ftpPath;
+                ftpWebRequest = (FtpWebRequest)FtpWebRequest.Create(new Uri(url));
+                ftpWebRequest.Credentials = new NetworkCredential(user, password);
+                ftpWebRequest.Method = WebRequestMethods.Ftp.ListDirectory;
+                ftpWebRequest.KeepAlive = false;
+                webResponse = ftpWebRequest.GetResponse();
+                reader = new StreamReader(webResponse.GetResponseStream());
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    if (line == ftpName)
+                    {
+                        success = true;
+                        break;
+                    }
+                    line = reader.ReadLine();
+                }
+            }
+            catch (Exception)
+            {
+                success = false;
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+                if (webResponse != null)
+                {
+                    webResponse.Close();
+                }
+            }
+            return success;
+        }
+
+        /// <summary>
+        /// If the function returns true, it indicates that the folder does not exist, and will be created.
+        /// </summary>
+        public bool FolderCheckExist(string ftpPath, string ftpDirName)
+        {
+            bool success = false;
+            FtpWebRequest ftpWebRequest = null;
+            WebResponse webResponse = null;
+            Stream ftpResponseStream = null;
+            try
+            {
+                string url = rootURL + ftpPath + ftpDirName;
+                ftpWebRequest = (FtpWebRequest)FtpWebRequest.Create(new Uri(url));
+                ftpWebRequest.Credentials = new NetworkCredential(user, password);
+                ftpWebRequest.UseBinary = true;
+                ftpWebRequest.Method = WebRequestMethods.Ftp.MakeDirectory;
+                FtpWebResponse response = (FtpWebResponse)ftpWebRequest.GetResponse();
+                ftpResponseStream = response.GetResponseStream();
+                success = true;
+            }
+            catch (Exception)
+            {
+                success = false;
+            }
+            finally
+            {
+                if (ftpResponseStream != null)
+                {
+                    ftpResponseStream.Close();
+                }
+                if (webResponse != null)
+                {
+                    webResponse.Close();
+                }
+            }
+            return success;
+        }
+    }
+}
---
+++ DataNexus/ComNet/MySQLEngine.cs
@@ -0,0 +1,207 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataNexus
+{
+    public class MySQLEngine
+    {
+        public string port { get; set; }
+        public string user { get; set; }
+        public string password { get; set; }
+
+        /// <summary>
+        /// _port:3306<para/>
+        /// _user:root<para/>
+        /// _password:Asher19910930
+        /// </summary>
+        public MySQLEngine(string _port, string _user, string _password)
+        {
+            port = _port;
+            user = _user;
+            password = _password;
+        }
+
+        private string StrPack()
+        {
+            return "server=127.0.0.1;" + nameof(port) + "=" + port + ";" + nameof(user) + "=" + user + ";" + nameof(password) + "=" + password + ";";
+        }
+
+        #region Show Database Information
+        public void ShowDatabase()
+        {
+            string connetStr = StrPack();
+            MySqlConnection conn = new MySqlConnection(connetStr);
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("show databases;", conn);
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    Console.WriteLine("Databases:");
+                    while (reader.Read())
+                    {
+                        string databaseName = reader.GetString(0);
+                        Console.WriteLine(databaseName);
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public void ShowTable(string database_name)
+        {
+            string connetStr = StrPack();
+            MySqlConnection conn = new MySqlConnection(connetStr);
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("use " + database_name + ";" + "show tables;", conn);
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    Console.WriteLine("Table:");
+                    while (reader.Read())
+                    {
+                        string databaseName = reader.GetString(0);
+                        Console.WriteLine(databaseName);
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        #endregion
+
+        #region Database Operation
+        public string CreateDatabase(string database_name)
+        {
+            return "create database " + database_name + ";";
+        }
+
+        public string DropDatabase(string database_name)
+        {
+            return "drop database " + database_name + ";";
+        }
+
+        public void MySQLDatabase(string database_name, Func<string, string> fun)
+        {
+            string connetStr = StrPack();
+            MySqlConnection conn = new MySqlConnection(connetStr);
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(fun(database_name), conn);
+                cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+        #endregion
+
+        #region Table Operation
+        public string CreateTable(string database_name, string table_name, string content)
+        {
+            return "use " + database_name + ";" +
+                   "create table " + table_name + "(" + content + ");";
+        }
+
+        public string AddTableColumn(string database_name, string table_name, string content)
+        {
+            return "use " + database_name + ";" +
+                   "alter table " + table_name + "add" + "(" + content + ");";
+        }
+
+        public string DeleteTableColumn(string database_name, string table_name, string column)
+        {
+            return "use " + database_name + ";" +
+                   "alter table " + table_name + "drop" + column + ";";
+        }
+
+        public string AddTableData(string database_name, string table_name, string data)
+        {
+            return "use " + database_name + ";" +
+                   "insert into " + table_name + "values" + "(" + data + ");";
+        }
+
+        public string DeleteTableData(string database_name, string table_name, string condition)
+        {
+            return "use " + database_name + ";" +
+                   "delete from " + table_name + "where" + condition + ";";
+        }
+
+        public void MySQLTable(string database_name, string table_name, string content, Func<string, string, string, string> fun)
+        {
+            string connetStr = StrPack();
+            MySqlConnection conn = new MySqlConnection(connetStr);
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(fun(database_name, table_name, content), conn);
+                cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+
+        public string DropTable(string database_name, string table_name)
+        {
+            return "use " + database_name + ";" +
+                   "drop table " + table_name + ";";
+        }
+
+        public void MySQLTable(string database_name, string table_name, Func<string, string, string> fun)
+        {
+            string connetStr = StrPack();
+            MySqlConnection conn = new MySqlConnection(connetStr);
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(fun(database_name, table_name), conn);
+                cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+        #endregion
+
+
+
+    }
+}
